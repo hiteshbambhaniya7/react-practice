70dc632c9724273ffbe5581243e5baeb99f2dd61
hiteshbambhaniya7 <bambhaniyahitesh7@gmail.com>
2023-07-26 10:13:46 +0530

    live time and date using Interval

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function getCurrentDateTime()
+{
+    var now = new Date();
+    var output = (<div class='container'>
+        <div class='row'>
+            <div class='col-lg-3'>
+                <h3>{now.toLocaleTimeString()}</h3>
+                <hr/>
+                <h5>{now.getDate()} - {now.getMonth()+1} - {now.getFullYear()}</h5>
+            </div>
+        </div>
+    </div>);
+    root.render(output);
+}
+setInterval(getCurrentDateTime,1000);
